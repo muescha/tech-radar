c00ac9d9653d277160c3ef375c9a95b0c4646e67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-03 01:30:32 +0000

    Deploy yaneek/tech-radar to github.com/yaneek/tech-radar.git:gh-pages

---
+++ precache-manifest.4fa3a80ff3e2c8b577dcaadf376ad454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "336854f248d5f9d13233",
+    "url": "/tech-radar/static/js/runtime~main.336854f2.js"
+  },
+  {
+    "revision": "d9a734f05ef419ca0ace",
+    "url": "/tech-radar/static/js/main.d9a734f0.chunk.js"
+  },
+  {
+    "revision": "1cb0dc1d3494d0e3675c",
+    "url": "/tech-radar/static/js/1.1cb0dc1d.chunk.js"
+  },
+  {
+    "revision": "d9a734f05ef419ca0ace",
+    "url": "/tech-radar/static/css/main.3191069d.chunk.css"
+  },
+  {
+    "revision": "a00f08e0faccd7d81fe6a18cff11d202",
+    "url": "/tech-radar/index.html"
+  }
+];
